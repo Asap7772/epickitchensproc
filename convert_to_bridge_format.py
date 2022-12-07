bee8a569663a103f860d259402a27448284c2d5b
Anikait Singh <singh.anikait@gmail.com>
2022-12-07 15:58:48 -0800

    formatted epic kitchen dataa

--- convert_to_bridge_format.py
+++ convert_to_bridge_format.py
@@ -2,10 +2,18 @@ import numpy as np
 import os
 from collections import defaultdict
 import tqdm
-from multiprocessing import Pool
+from multiprocessing import Pool, Manager
+import argparse
 
-annotation_path = 'anns/EPIC_100_train.pkl'
-data = np.load(annotation_path, allow_pickle=True)
+parser = argparse.ArgumentParser()
+parser.add_argument('--annotation_path', type=str, default='anns/EPIC_100_train.pkl')
+parser.add_argument('--path_to_frames', type=str, default='/raid/asap7772/epic100/frames')
+parser.add_argument('--output_path', type=str, default='/raid/asap7772/epic100/output')
+parser.add_argument('--num_workers', type=int, default=64)
+parser.add_argument('--max_frames', type=int, default=-1)
+args = parser.parse_args()
+
+data = np.load(args.annotation_path, allow_pickle=True)
 
 data['noun_verb'] = data[['noun', 'verb']].agg('-'.join, axis=1)
 vc = data['noun_verb'].value_counts()
@@ -16,9 +24,6 @@ new_data = data[data['noun_verb'].isin(des_labels)]
 sorted_data = new_data.sort_values(by=['noun_verb'])
 
 row_num = 0
-dict_traj = defaultdict(list)
-path_to_frames='/raid/asap7772/epic100/frames'
-
 filter_tasks =  []
 if len(filter_tasks) > 0:
     print('Filtering tasks', filter_tasks)
@@ -26,25 +31,44 @@ if len(filter_tasks) > 0:
 else:
     print('No filter tasks')
     
-    
-for i in tqdm.tqdm(range(len(sorted_data))):
+dict_traj = {k: Manager().list() for k in des_labels}
+
+def process_row(i):
     ind = sorted_data.index[i]
-    folder_path= os.path.join(path_to_frames, sorted_data['participant_id'][ind], 'rgb_frames', str(sorted_data['video_id'][ind]))
+    folder_path= os.path.join(args.path_to_frames, sorted_data['participant_id'][ind], 'rgb_frames', str(sorted_data['video_id'][ind]))
     frame_names = sorted(os.listdir(folder_path))
     filter_value = lambda x: sorted_data['start_frame'][ind] <= int(x.split('.')[0].split('_')[-1]) <= sorted_data['stop_frame'][ind]
     filtered_frame_names = list(filter(filter_value, frame_names))
     full_frame_names = [os.path.join(folder_path, frame_name) for frame_name in filtered_frame_names]
     
-    assert len(filtered_frame_names) == sorted_data['stop_frame'][ind] - sorted_data['start_frame'][ind] + 1, 'Number of frames in the folder does not match the number of frames in the annotation'
-    assert os.path.exists(folder_path) and all(map(os.path.exists, full_frame_names)), f'Folder {folder_path} does not exist'
-    
     aux_data = sorted_data.iloc[i]
+    task = aux_data['noun_verb']
     
-    dict_traj[sorted_data['noun_verb'][ind]].append((full_frame_names, aux_data))
-    
-print('Number of tasks', len(dict_traj.keys()))
-print('Number of videos', sum([len(dict_traj[key]) for key in dict_traj.keys()]))
-print('Number of frames', sum([len(dict_traj[key][i][0]) for key in dict_traj.keys() for i in range(len(dict_traj[key]))]))
+    dict_traj[task].append((full_frame_names, aux_data))
 
-output_path='/raid/asap7772/epic100/output'
-np.save(f'{output_path}/epic100_train_dict.npy', dict_traj)
+
+if args.max_frames > 0:
+    num_rows = args.max_frames
+else:
+    num_rows = sorted_data.shape[0]
+
+num_workers = args.num_workers
+pool = Pool(num_workers)
+tqdm.tqdm.write('Processing data')
+tqdm.tqdm.write('Number of workers: {}'.format(num_workers))
+tqdm.tqdm.write('Number of tasks: {}'.format(num_rows))
+tqdm.tqdm.write('Number of tasks per worker: {}'.format(num_rows // num_workers))
+
+for _ in tqdm.tqdm(pool.imap_unordered(process_row, range(num_rows)), total=num_rows):
+    pass
+pool.close()
+pool.join()
+
+for k in dict_traj.keys():
+    dict_traj[k] = list(dict_traj[k])
+
+
+for k in dict_traj.keys():
+    print(k, len(dict_traj[k]))
+    
+np.save(f'{args.output_path}/epic100_train_dict.npy', dict_traj)
---
+++ frames_to_bridge_format.py
@@ -0,0 +1,80 @@
+import numpy as np
+import os
+from collections import defaultdict
+import tqdm
+from multiprocessing import Pool, Manager
+import argparse
+from PIL import Image
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--npy_path', type=str, default='/raid/asap7772/epic100/output/epic100_train_dict.npy')
+parser.add_argument('--path_to_frames', type=str, default='/raid/asap7772/epic100/frames')
+parser.add_argument('--output_path', type=str, default='/raid/asap7772/epic100/epic100_bridgeform')
+parser.add_argument('--num_workers', type=int, default=16)
+parser.add_argument('--train', type=int, default=1)
+args = parser.parse_args()
+
+desired_keys = ['observations', 'next_observations', 'actions', 'rewards', 'terminals', 'aux_data']
+
+data = np.load(args.npy_path, allow_pickle=True).item()
+
+tasks = list(data.keys())
+
+def process_task(task):
+    print('Processing task', task)
+    # one task is a list of (frame_paths, aux_data)
+    # create a numpy file for each task
+    output_dict = {k: [] for k in desired_keys}
+    output_rew = []
+    sub_folder = 'train' if args.train else 'val'
+    
+    for frame_paths, aux_data in tqdm.tqdm(data[task]):
+        folder_path = os.path.join(args.output_path, task, sub_folder)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path, exist_ok=True)
+    
+        all_observations = []
+        all_actions = []
+        all_rewards = []
+        all_terminals = []
+        all_aux_data = []
+        # frame_path is a jpg file
+        for frame_path in frame_paths:
+            frame = np.array(Image.open(frame_path))
+            
+            obs_dict = {
+                'images0': frame,
+                'images1': frame,
+                'images2': frame,
+                'state': np.zeros(7),
+            }
+            
+            all_observations.append(obs_dict)
+            all_actions.append(np.zeros(7))
+            all_rewards.append(-1.0)
+            all_terminals.append(0)
+            all_aux_data.append(aux_data)
+        
+        observations, next_observations = all_observations[:-1], all_observations[1:]
+        actions, rewards, terminals, aux_data = all_actions[:-1], all_rewards[:-1], all_terminals[:-1], all_aux_data[:-1]
+        
+        output_dict['observations'].append(observations)
+        output_dict['next_observations'].append(next_observations)
+        output_dict['actions'].append(actions)
+        output_dict['rewards'].append(rewards)
+        output_dict['terminals'].append(terminals)
+        output_dict['aux_data'].append(aux_data)
+        output_rew.append(rewards)
+
+    output_path = os.path.join(args.output_path, task, sub_folder, 'out.npy')
+    output_path_rew = os.path.join(args.output_path, task, sub_folder, 'out_rew.npy')
+    
+    np.save(output_path, output_dict)
+    np.save(output_path_rew, output_rew)
+    
+    print('Saved to', output_path)
+
+print('Processing', len(tasks), 'tasks')
+
+with Pool(args.num_workers) as p:
+    p.map(process_task, tasks)
